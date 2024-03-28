18e3fdf6175ce247a22fa211ac813a7e6f8ac373
Piotr <khazal@wp.pl>
2024-03-28 07:29:08 +0100

    Upgrade 1.6.2

--- app/components/EnvatoGrid.tsx
+++ app/components/EnvatoGrid.tsx
@@ -43,9 +43,9 @@ const EnvatoGrid: React.FC = () => {
         <div className="-mt-16 flex justify-center md:justify-end">
           <img className="h-20 w-20 rounded-full border-8 border-white border-opacity-40 object-cover" alt='envato-logo-icon128x128' src='/images/logos/envato-logo-icon128x128.png' />
         </div>
-        <h2 className="mt-2 text-xl md:text-2xl lg:text-xl md:mt-0 bg-gradient-to-tl from-lime-600 via-lime-700 to-lime-900 bg-clip-text text-transparent line-clamp-3 text-ellipsis min-h-[3rem]">Templates</h2>
         <Image src="/images/logos/EnvatoMarket-Themeforest-Dark.png" alt="EnvatoMarket-Themeforest-Dark" width={427} height={82} />
-        <p className="text-sm md:text-lg lg:text-lg mt-2 text-lime-800">Find Your theme</p>
+        <h2 className="mt-2 text-md md:text-md lg:text-md md:mt-0 bg-gradient-to-bl from-lime-700 via-lime-800 to-lime-800 bg-clip-text text-transparent">Professional WordPress Themes & Website Templates for any project</h2>
+        <p className="text-sm md:text-md lg:text-md mt-2 text-lime-800">Discover of easy to customize themes, templates & CMS products, made by world-class developers</p>
       </div>
       </Link>
       <Link href='/codecanyon'>
@@ -53,9 +53,9 @@ const EnvatoGrid: React.FC = () => {
         <div className="-mt-16 flex justify-center md:justify-end">
           <img className="h-20 w-20 rounded-full border-8 border-white border-opacity-40 object-cover" alt='envato-logo-icon128x128' src='/images/logos/envato-logo-icon128x128.png' />
         </div>
-        <h2 className="mt-2 text-xl md:text-xl lg:text-xl md:mt-0 bg-gradient-to-tl from-lime-600 via-lime-700 to-lime-900 bg-clip-text text-transparent line-clamp-3 text-ellipsis min-h-[3rem]">Templates</h2>
         <Image src="/images/logos/EnvatoMarket-Codecanyon-Dark.png" alt="EnvatoMarket-Codecanyon-Dark" width={393} height={82} />
-        <p className="text-sm md:text-lg lg:text-lg mt-2 text-lime-800">Find Your theme</p>
+        <h2 className="mt-2 text-md md:text-md lg:text-md md:mt-0 text-lime-900">Code, scripts & plugins for every website build</h2>
+        <p className="text-sm md:text-md lg:text-md mt-2 text-lime-800">Choose from ecommerce plugins, mobile app templates, PHP, Bootstrap & more for any budget, built by the world’s best developers</p>
       </div>
       </Link>
       <Link href='/graphicriver'>
@@ -63,9 +63,9 @@ const EnvatoGrid: React.FC = () => {
         <div className="-mt-16 flex justify-center md:justify-end">
           <img className="h-20 w-20 rounded-full border-8 border-white border-opacity-40 object-cover" alt='envato-logo-icon128x128' src='/images/logos/envato-logo-icon128x128.png' />
         </div>
-        <h2 className="mt-2 text-xl md:text-xl lg:text-xl md:mt-0 bg-gradient-to-tl from-lime-600 via-lime-700 to-lime-900 bg-clip-text text-transparent line-clamp-3 text-ellipsis min-h-[3rem]">Templates</h2>
         <Image src="/images/logos/EnvatoMarket-Graphicriver-Dark.png" alt="EnvatoMarket-Graphicriver-Dark" width={338} height={82} />
-        <p className="text-sm md:text-lg lg:text-lg mt-2 text-lime-800">Find Your theme</p>
+        <h2 className="mt-2 text-md md:text-md lg:text-md md:mt-0 bg-gradient-to-tl from-lime-600 via-lime-700 to-lime-900 bg-clip-text text-transparent">Discover fonts, logos & presentations</h2>
+        <p className="text-sm md:text-md lg:text-md mt-2 text-lime-800">Discover our huge collection of hand-reviewed graphic assets from our community of designers.</p>
       </div>
       </Link>
       <Link href='/photodune'>
@@ -73,9 +73,9 @@ const EnvatoGrid: React.FC = () => {
         <div className="-mt-16 flex justify-center md:justify-end">
           <img className="h-20 w-20 rounded-full border-8 border-white border-opacity-40 object-cover" alt='envato-logo-icon128x128' src='/images/logos/envato-logo-icon128x128.png' />
         </div>
-        <h2 className="mt-2 text-xl md:text-xl lg:text-xl md:mt-0 bg-gradient-to-tl from-lime-600 via-lime-700 to-lime-900 bg-clip-text text-transparent line-clamp-3 text-ellipsis min-h-[3rem]">Templates</h2>
         <Image src="/images/logos/EnvatoMarket-Photodune-Dark.png" alt="EnvatoMarket-EnvatoMarket-Photodune-Dark-Dark" width={338} height={82} />
-        <p className="text-sm md:text-lg lg:text-lg mt-2 text-lime-800">Find Your theme</p>
+        <h2 className="mt-2 text-md md:text-md lg:text-md md:mt-0 bg-gradient-to-tl from-lime-600 via-lime-700 to-lime-900 bg-clip-text text-transparent">Discover our collection of royalty free stock images from our community of photographers.</h2>
+        <p className="text-sm md:text-md lg:text-md mt-2 text-lime-800">Top quality stock images for your next project</p>
       </div>
       </Link>
       <Link href='/videohive'>
@@ -83,9 +83,9 @@ const EnvatoGrid: React.FC = () => {
         <div className="-mt-16 flex justify-center md:justify-end">
           <img className="h-20 w-20 rounded-full border-8 border-white border-opacity-40 object-cover" alt='envato-logo-icon128x128' src='/images/logos/envato-logo-icon128x128.png' />
         </div>
-        <h2 className="mt-2 text-xl md:text-xl lg:text-xl md:mt-0 bg-gradient-to-tl from-lime-600 via-lime-700 to-lime-900 bg-clip-text text-transparent line-clamp-3 text-ellipsis min-h-[3rem]">Templates</h2>
         <Image src="/images/logos/EnvatoMarket-Videohive-Dark.png" alt="EnvatoMarket-Videohive-Dark" width={338} height={82} />
-        <p className="text-sm md:text-lg lg:text-lg mt-2 text-lime-800">Find Your theme</p>
+        <h2 className="mt-2 text-md md:text-md lg:text-md md:mt-0 bg-gradient-to-tl from-lime-600 via-lime-700 to-lime-900 bg-clip-text text-transparent">Stock Footage, Video Effects & Video Templates</h2>
+        <p className="text-sm md:text-md lg:text-md mt-2 text-lime-800">Royalty videos and templates created by video professionals from all over the world.</p>
       </div>
       </Link>
       <Link href='/audiojungle'>
@@ -93,14 +93,23 @@ const EnvatoGrid: React.FC = () => {
         <div className="-mt-16 flex justify-center md:justify-end">
           <img className="h-20 w-20 rounded-full border-8 border-white border-opacity-40 object-cover" alt='envato-logo-icon128x128' src='/images/logos/envato-logo-icon128x128.png' />
         </div>
-        <h2 className="mt-2 text-xl md:text-xl lg:text-xl md:mt-0 bg-gradient-to-tl from-lime-600 via-lime-700 to-lime-900 bg-clip-text text-transparent line-clamp-3 text-ellipsis min-h-[3rem]">Templates</h2>
         <Image src="/images/logos/EnvatoMarket-Audiojungle-Dark.png" alt="EnvatoMarket-Audiojungle-Dark" width={338} height={82} />
-        <p className="text-sm md:text-lg lg:text-lg mt-2 text-lime-800">Find Your theme</p>
+        <h2 className="mt-2 text-md md:text-md lg:text-md md:mt-0 bg-gradient-to-tl from-lime-600 via-lime-700 to-lime-900 bg-clip-text text-transparent">Tracks and sounds from our community of musicians and sound engineers.</h2>
+        <p className="text-sm md:text-md lg:text-md mt-2 text-lime-800">Royalty free music and audio tracks</p>
       </div>
       </Link>
+</div>
+
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+
+
 
 
 </div>
+
+
+
+
 
 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
       {items.length > 0 ? (
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import EnvatoGrid from './components/EnvatoGrid';
 import Footer from './components/Footer';
 import 'tailwindcss/tailwind.css';
 import NavBar from './components/NavBar';
-
+import LinkTextLight from './components/LinkTextLight'
 
 
 export default function Home() {
@@ -11,7 +11,23 @@ export default function Home() {
     <main className="container mx-auto p-4">
       <NavBar/>
       <div className="container mx-auto p-4">
-        <p className='text-2xl md:text-3xl lg:text-4xl bg-gradient-to-tl from-lime-600 via-lime-700 to-lime-900 bg-clip-text text-transparent'>Random new files</p>
+        <div className="mt-6 mb-5 w-12/12 rounded-lg px-4 py-2 shadow-lg md:px-8 md:py-4 bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-lime-400 via-lime-500 to-lime-700">
+          <div>
+            <h2 className="text-2xl md:text-3xl lg:text-4xl bg-gradient-to-bl from-lime-600 via-lime-700 to-lime-800 bg-clip-text text-transparent">Find Your style in Envato Market</h2>
+            <p className='text-xs md:text-md lg:text-lg mt-1 text-lime-900'>Explore a collection of top-quality assets</p>
+          </div>
+          <div className='flex flex-col md:flex-row lg:flex-row mt-2'>
+            <div className="w-6/6 md:w-2/6 lg:w-2/6">
+            <img className="w-full object-cover" alt='' src="/images/logos/EnvatoMarket-Logo-Trans-White.png" />
+            </div>
+            <div className='w-6/6 md:w-4/6 lg:w-4/6'>
+
+            </div>
+          </div>
+          <div className='mt-2'>
+            <LinkTextLight to='/' fontSize='text-xs md:text-xs lg:text-xs'>Start</LinkTextLight>
+          </div>
+        </div>
       </div>
       <div className="container mx-auto p-4">
         <EnvatoGrid />
